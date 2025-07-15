cce2ad55ad7c4745f7d535fc8ba6ad4c96ad6a2c
khachik_zahalyan <zahalyanxcho@gmail.com>
2025-07-15 15:51:04 +0400

    Minor changes

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { loginUser, saveUser } from "../../utils/auth";
-import "./Login.css";
+import "./login.css";
 
 export default function Login() {
   const [username, setUsername] = useState("");
